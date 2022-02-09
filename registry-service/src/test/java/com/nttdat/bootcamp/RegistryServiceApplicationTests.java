cc75ab60f8c97a54cc4116fb28807e7fe5b1a216
inmamesa <77283757+inmamesa@users.noreply.github.com>
2022-02-09 12:53:02 +0100

    registry-service

---
+++ registry-service/src/test/java/com/nttdat/bootcamp/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdat.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
